45b0bcb5067560304ea17c7d9249808b62b17fa2
mmihye <smilehye910@naver.com>
2023-08-17 17:33:30 +0900

    FEAT/Create LoadFragment

---
+++ app/src/main/java/com/capjjang/rightnow/ui/LoadFragment.kt
@@ -0,0 +1,25 @@
+package com.capjjang.rightnow.ui
+
+import com.android.example.travalue.base.BaseFragment
+import com.capjjang.rightnow.R
+import com.capjjang.rightnow.databinding.FragmentLoadBinding
+
+class LoadFragment  : BaseFragment<FragmentLoadBinding>(R.layout.fragment_load) {
+
+    override fun initStartView() {
+        super.initStartView()
+    }
+
+    override fun initDataBinding() {
+        super.initDataBinding()
+
+    }
+
+
+    override fun initAfterBinding() {
+        super.initAfterBinding()
+
+    }
+
+
+}
