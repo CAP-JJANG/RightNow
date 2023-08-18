d3554a896879249addab4e7613a006a607fc25ae
mmihye <smilehye910@naver.com>
2023-08-18 23:46:00 +0900

    FEAT/Add Check Permission

--- app/src/main/java/com/capjjang/rightnow/ui/StartFragment.kt
+++ app/src/main/java/com/capjjang/rightnow/ui/StartFragment.kt
@@ -1,5 +1,9 @@
 package com.capjjang.rightnow.ui
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import com.capjjang.rightnow.R
 import com.capjjang.rightnow.base.BaseFragment
 import com.capjjang.rightnow.databinding.FragmentStartBinding
@@ -10,6 +14,9 @@ class StartFragment : BaseFragment<FragmentStartBinding>(R.layout.fragment_start
     override fun initStartView() {
         super.initStartView()
 
+
+        // 권한 확인
+        onCheckPermission()
 
     }
 
@@ -28,4 +35,53 @@ class StartFragment : BaseFragment<FragmentStartBinding>(R.layout.fragment_start
     }
 
 
+    private fun onCheckPermission() {
+        if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(
+                requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    requireActivity(),
+                    Manifest.permission.RECORD_AUDIO,
+                )
+            ) {
+                Toast.makeText(context, "앱 실행을 위해서는 권한을 설정해야 합니다.", Toast.LENGTH_SHORT).show()
+                ActivityCompat.requestPermissions(
+                    requireActivity(),
+                    arrayOf(
+                        Manifest.permission.RECORD_AUDIO,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    ),
+                    PERMISSIONS_REQUEST
+                )
+            } else {
+                ActivityCompat.requestPermissions(
+                    requireActivity(),
+                    arrayOf(
+                        Manifest.permission.RECORD_AUDIO,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    ), PERMISSIONS_REQUEST
+                )
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            PERMISSIONS_REQUEST -> {
+                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(context, "앱 실행을 위한 권한이 설정 되었습니다", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(context, "앱 실행을 위한 권한이 취소 되었습니다", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+        }
+    }
 }
