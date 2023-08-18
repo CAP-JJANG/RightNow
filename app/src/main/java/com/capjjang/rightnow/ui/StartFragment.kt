4032b9c7db00f9477e21506d150d1f0af9233258
mmihye <smilehye910@naver.com>
2023-08-18 23:23:00 +0900

    FEAT/Create StartFragment

---
+++ app/src/main/java/com/capjjang/rightnow/ui/StartFragment.kt
@@ -0,0 +1,39 @@
+package com.capjjang.rightnow.ui
+
+import com.capjjang.rightnow.R
+import com.capjjang.rightnow.databinding.FragmentStartBinding
+
+package com.example.rightnow.ui
+
+import android.content.pm.PackageManager
+import android.Manifest
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.android.example.travalue.base.BaseFragment
+import com.example.rightnow.R
+import com.example.rightnow.databinding.FragmentStartBinding
+
+class StartFragment : BaseFragment<FragmentStartBinding>(R.layout.fragment_start) {
+
+    override fun initStartView() {
+        super.initStartView()
+
+
+    }
+
+
+    override fun initDataBinding() {
+        super.initDataBinding()
+
+
+    }
+
+
+    override fun initAfterBinding() {
+        super.initAfterBinding()
+
+
+    }
+
+
+}
