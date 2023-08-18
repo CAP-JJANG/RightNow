3ca140d165658a83afc2cfa81703395361587b45
mmihye <smilehye910@naver.com>
2023-08-19 00:09:00 +0900

    FEAT/Create QuizFragment

--- app/src/main/java/com/capjjang/rightnow/ui/QuizFragment.kt
+++ app/src/main/java/com/capjjang/rightnow/ui/QuizFragment.kt
@@ -1,4 +1,26 @@
 package com.capjjang.rightnow.ui
 
-class QuizFragment {
+import com.capjjang.rightnow.R
+import com.capjjang.rightnow.base.BaseFragment
+import com.capjjang.rightnow.databinding.FragmentQuizBinding
+
+class QuizFragment : BaseFragment<FragmentQuizBinding>(R.layout.fragment_quiz) {
+
+
+    override fun initStartView() {
+        super.initStartView()
+    }
+
+    override fun initDataBinding() {
+        super.initDataBinding()
+
+    }
+
+
+    override fun initAfterBinding() {
+        super.initAfterBinding()
+
+    }
+
+
 }
