34fb55066b0af4d1636398a441faf0608e5054c6
mmihye <smilehye910@naver.com>
2023-08-21 20:07:00 +0900

    FEAT/Create QuizGrade Fragment & Layout

---
+++ app/src/main/java/com/capjjang/rightnow/ui/QuizGradeFragment.kt
@@ -0,0 +1,25 @@
+package com.capjjang.rightnow.ui
+
+import com.capjjang.rightnow.R
+import com.capjjang.rightnow.base.BaseFragment
+import com.capjjang.rightnow.databinding.FragmentQuizGradeBinding
+
+class QuizGradeFragment: BaseFragment<FragmentQuizGradeBinding>(R.layout.fragment_quiz_grade) {
+
+    override fun initStartView() {
+        super.initStartView()
+    }
+
+    override fun initDataBinding() {
+        super.initDataBinding()
+
+
+    }
+
+
+    override fun initAfterBinding() {
+        super.initAfterBinding()
+
+    }
+
+}
