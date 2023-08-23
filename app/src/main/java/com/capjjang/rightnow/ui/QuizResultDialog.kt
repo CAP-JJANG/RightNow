6d1435bdeac840db6d14de16f465878bd57ca412
mmihye <smilehye910@naver.com>
2023-08-23 21:35:00 +0900

    FEAT/Create QuizResult Dialog & Layout

---
+++ app/src/main/java/com/capjjang/rightnow/ui/QuizResultDialog.kt
@@ -0,0 +1,24 @@
+package com.capjjang.rightnow.ui
+
+import android.view.View
+import androidx.navigation.fragment.navArgs
+import com.capjjang.rightnow.R
+import com.capjjang.rightnow.base.BaseBottomDialogFragment
+import com.capjjang.rightnow.databinding.DialogQuizResultBinding
+
+class QuizResultDialog: BaseBottomDialogFragment<DialogQuizResultBinding>(R.layout.dialog_quiz_result) {
+
+    override fun initStartView() {
+        super.initStartView()
+    }
+
+    override fun initDataBinding() {
+        super.initDataBinding()
+
+    }
+
+    override fun initAfterBinding() {
+        super.initAfterBinding()
+
+    }
+}
