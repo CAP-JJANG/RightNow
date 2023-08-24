e079e169468ed4de77bb2ec87dec9a74b9c3cb81
mmihye <smilehye910@naver.com>
2023-08-25 00:39:00 +0900

    FEAT/Create AudioRecorder

---
+++ app/src/main/java/com/capjjang/rightnow/util/AudioRecorder.kt
@@ -0,0 +1,85 @@
+package com.capjjang.rightnow.util
+import android.content.Context
+import android.media.MediaRecorder
+import android.util.Log
+import com.capjjang.rightnow.model.RecordModel
+import com.example.rightnow.apiManager.RecordApiManager
+import java.io.IOException
+
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileInputStream
+
+class AudioRecorder {
+    private var mediaRecorder: MediaRecorder? = null
+    var isRecording = true
+    private var fileName = ""
+
+    fun startRecording(outputFile: String, apiManager: RecordApiManager) {
+        isRecording =true
+
+        mediaRecorder = MediaRecorder().apply {
+            setAudioSource(MediaRecorder.AudioSource.MIC)
+            setOutputFormat(MediaRecorder.OutputFormat.AAC_ADTS) // or MediaRecorder.OutputFormat.MPEG_4
+            setOutputFile(outputFile)
+            setAudioEncoder(MediaRecorder.AudioEncoder.AAC) // or MediaRecorder.AudioEncoder.DEFAULT
+            setAudioSamplingRate(44100) // set the desired sampling rate
+            setAudioEncodingBitRate(320000)
+            setMaxDuration(2000)
+
+            try {
+                prepare()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+            start()
+        }
+
+        Thread.sleep(2000)
+
+        // 서버 전송
+        Log.d("[mmihye]","녹음 멈춤 & 서버 전송")
+        val byteArray = mediaRecorderToByteArray(outputFile)
+        val recordModel = byteArray?.let { RecordModel(it) }
+        if (recordModel != null) {
+            apiManager.postTest(recordModel)
+        }
+
+    }
+
+
+    fun stopRecording() {
+
+        mediaRecorder?.apply {
+            stop()
+            release()
+        }
+        mediaRecorder = null
+        isRecording = false
+
+    }
+
+    fun mediaRecorderToByteArray(outputFile: String): ByteArray? {
+        val file = File(outputFile)
+        if (!file.exists()) {
+            return null
+        }
+
+        val inputStream = FileInputStream(file)
+        val buffer = ByteArrayOutputStream()
+
+        inputStream.use { input ->
+            buffer.use { output ->
+                val data = ByteArray(1024)
+                var count: Int
+                while (input.read(data).also { count = it } != -1) {
+                    output.write(data, 0, count)
+                }
+                output.flush()
+            }
+        }
+
+        return buffer.toByteArray()
+    }
+
+}
